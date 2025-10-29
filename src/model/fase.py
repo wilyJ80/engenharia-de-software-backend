05d0cdc2ee8ceaa6ec2bd723c3e38eaac662b933
Lucas Gomes <lucas@b2bflow.com.br>
2025-10-29 09:04:53 -0300

    feat: desenvolvimento do modelo Fase

---
+++ src/model/fase.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel, Field
+from typing import Optional
+from datetime import datetime
+
+class FaseBase(BaseModel):
+    nome : str = Field(description="Nome da fase", example="")
+    descricao: str = Field(description="Descrição da fase", example="")
+    artefato_id: str = Field(description="Id do artefato", example="")
+
+
+class FaseResponse(FaseBase):
+    id: str = Field(..., description="Identificador único da Fase")
+
+class Fase(FaseResponse):
+    created_at: datetime           = Field(..., description="Data de criação do registro")
+    updated_at: Optional[datetime] = Field(None, description="Data da última atualização do registro")
