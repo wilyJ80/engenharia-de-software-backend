13ce28ed0c6a3b13acd3fbae0c466b111f042a71
Lucas Gomes <lucas@b2bflow.com.br>
2025-10-29 09:30:20 -0300

    refactor: mudança de nome da variável descricao -> descritivo

--- src/model/fase.py
+++ src/model/fase.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from artefato import Artefato
 class FaseBase(BaseModel):
     nome : str = Field(description="Nome da fase", example="")
-    descricao: str = Field(description="Descrição da fase", example="")
+    descritivo: str = Field(description="Descrição da fase", example="")
     artefato: list[Artefato] = Field(description="Id do artefato", example="")
     ordem: int = Field(description="Ordem da fase", example="")
 
