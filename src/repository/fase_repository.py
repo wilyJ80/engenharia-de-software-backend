e8b4fda358a8b10742d6007175321df713836388
Lucas Gomes <lucas@b2bflow.com.br>
2025-10-29 09:42:17 -0300

    feat: adição do metodo get_fase_by_id

--- src/repository/fase_repository.py
+++ src/repository/fase_repository.py
@@ -1,5 +1,6 @@
 from psycopg2.extras import RealDictCursor
 from psycopg2.extensions import connection
+from utils.functions import print_error_details
 
 # |=======| LISTANDO TODOS AS FASES |=======|
 async def get_all_fases(conn: connection):
@@ -7,10 +8,23 @@ async def get_all_fases(conn: connection):
         cursor.execute("""
                         SELECT id, nome, descritivo, ordem FROM fase;
                     """)
-        
+
         return cursor.fetchall()
-    
+
 # FIND BY ID
+
+async def get_fase_by_id(conn: connection, fase_id: int):
+    with conn.cursor(cursor_factory=RealDictCursor) as cursor:
+        try:
+            cursor.execute("""
+                            SELECT id, nome, descritivo, ordem FROM fase WHERE id = %s;
+                        """)
+
+            cursor.execute(cursor, (fase_id,))
+            return cursor.fetchone()
+        except Exception as e:
+            print_error_details(e)
+
 
 # |=======| POST
 
