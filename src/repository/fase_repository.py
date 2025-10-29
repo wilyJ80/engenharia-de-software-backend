1302c4b14e6a36c51cc39b6a25f7059220c15fb4
Lucas Gomes <lucas@b2bflow.com.br>
2025-10-29 09:45:57 -0300

    feat: adição do metodo create_fase

--- src/repository/fase_repository.py
+++ src/repository/fase_repository.py
@@ -1,9 +1,11 @@
 from psycopg2.extras import RealDictCursor
 from psycopg2.extensions import connection
 from utils.functions import print_error_details
+from model.fase import FaseBase
+from psycopg2.extras import RealDictRow
 
 # |=======| LISTANDO TODOS AS FASES |=======|
-async def get_all_fases(conn: connection):
+async def get_all_fases(conn: connection) -> list[RealDictRow]:
     with conn.cursor(cursor_factory=RealDictCursor) as cursor:
         cursor.execute("""
                         SELECT id, nome, descritivo, ordem FROM fase;
@@ -13,7 +15,7 @@ async def get_all_fases(conn: connection):
 
 # FIND BY ID
 
-async def get_fase_by_id(conn: connection, fase_id: int):
+async def get_fase_by_id(conn: connection, fase_id: int) -> RealDictRow | None:
     with conn.cursor(cursor_factory=RealDictCursor) as cursor:
         try:
             cursor.execute("""
@@ -27,7 +29,16 @@ async def get_fase_by_id(conn: connection, fase_id: int):
 
 
 # |=======| POST
-
+async def create_fase(conn: connection, fase: FaseBase):
+    with conn.cursor(cursor_factory=RealDictCursor) as cursor:
+        try:
+            cursor.execute("""
+                            INSERT INTO fase (nome, descritivo, ordem) VALUES (%s, %s, %s) RETURNING id, nome, descritivo, ordem;
+                        """)
+            cursor.execute(cursor, (fase.nome, fase.descritivo, fase.ordem))
+            return cursor.fetchone()
+        except Exception as e:
+            print_error_details(e)
 # PUT
 
 # DELETE
