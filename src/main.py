dfeffb4edead92ebe6b3ee2397e9939c92d0e639
Lucas Gomes <lucas@b2bflow.com.br>
2025-10-29 11:31:06 -0300

    freat: adição da rotas mo main

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
-
+from routes.fase_routes import fase_router
 from routes.example_route import router as exampleRouter
 
 app = FastAPI()
 
 app.include_router(exampleRouter)
+app.include_router(fase_router)
